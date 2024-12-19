c600edfe282c25d78cde745534daabdada21756c
velkorra <anton.s.vertunov@gmail.com>
2024-12-19 19:24:14 +0300

    sadijsauyfsaudhcsa

--- services/userServivce.js
+++ services/userServivce.js
@@ -1,15 +1,15 @@
 class UserService {
-  constructor() {
-    this.userRepository = new UserRepository();
-  }
+    constructor() {
+        this.userRepository = new UserRepository();
+    }
 
-  async getUserById(id) {
-    return this.userRepository.getUserById(id);
-  }
+    async getUserById(id) {
+        return this.userRepository.getUserById(id);
+    }
 
-  async createUser(user) {
-    return this.userRepository.createUser(user);
-  }
+    async createUser(user) {
+        return this.userRepository.createUser(user);
+    }
 
     async updateUser(user) {
         return this.userRepository.updateUser(user);
